9df7e54aa67ed4d71761a7a1cc2b97167430d51e
ndvo <nelson@ocastudios.com>
2018-11-17 18:57:30 -0200

    Fixing production relative paths

--- prototype/001_draft/build/service-worker.js
+++ prototype/001_draft/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da83598b302135bbb95ecf626c37c78a.js"
+  "/prototype/001_draft/precache-manifest.e2688756cf4ac4705071bb314a4a6737.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/prototype/001_draft/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
